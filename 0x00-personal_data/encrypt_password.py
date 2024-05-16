338c060f9ce11be08a2e117c0ae64d6ea07bbee3
kelvinthuo999 <thoukelvinngigi@gmail.com>
2024-05-16 10:08:59 +0000

    create: function to encrypt a password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+This module contains a function to hash passwords.
+"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashes a password using bcrypt.
+
+    Args:
+        password (str): The password to hash.
+
+    Returns:
+        bytes: The hashed password.
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
