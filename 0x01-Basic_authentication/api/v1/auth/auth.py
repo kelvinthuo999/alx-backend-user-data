a9ba6e07216d0799acd5cfc337e83c22d6d1ce25
kelvinthuo999 <thoukelvinngigi@gmail.com>
2024-05-21 11:24:57 +0000

    create: method to identify paths that do not require auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,14 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Method to check if authentication is required."""
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        for excluded_path in excluded_paths:
+            if path.rstrip('/') == excluded_path.rstrip('/'):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Method to get the authorization header."""
