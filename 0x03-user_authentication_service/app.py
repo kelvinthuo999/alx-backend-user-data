3fb9ae74fd0ef859b8ccd0a7fff395da45239a07
kelvinthuo999 <thoukelvinngigi@gmail.com>
2024-05-29 06:09:40 +0000

    create: endpoint to create new user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
 """
-Basic Flask app
+Basic Flask app with user registration endpoint
 """
 
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 @app.route("/", methods=["GET"])
 def welcome():
@@ -14,6 +16,21 @@ def welcome():
     GET route that returns a JSON payload.
     """
     return jsonify({"message": "Bienvenue"})
+
+@app.route("/users", methods=["POST"])
+def users():
+    """
+    POST /users route to register a user.
+    Expects form data fields: "email" and "password".
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
