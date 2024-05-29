5942e6415333b9f8930b9a201fd8b9ef71e467e3
kelvinthuo999 <thoukelvinngigi@gmail.com>
2024-05-29 05:59:02 +0000

    create: update user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,6 +7,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
@@ -34,6 +36,7 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """
         Add a new user to the database.
+
         :param email: The email of the user.
         :param hashed_password: The hashed password of the user.
         :return: The newly created User object.
@@ -42,3 +45,34 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Find a user by arbitrary keyword arguments.
+
+        :param kwargs: Arbitrary keyword arguments to filter the query.
+        :return: The first User object that matches the query.
+        :raises NoResultFound: If no user is found.
+        :raises InvalidRequestError: If the query is invalid.
+        """
+        try:
+            return self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound("No user found with the specified attributes")
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid query parameters")
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        Update a user's attributes.
+
+        :param user_id: The ID of the user to update.
+        :param kwargs: Arbitrary keyword arguments to update the user's attributes.
+        :raises ValueError: If an attribute that does not correspond to a User attribute is passed.
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError(f"Attribute {key} does not exist on User")
+            setattr(user, key, value)
+        self._session.commit()
